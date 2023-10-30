0e9aff007eab4927b524dbea5eb5582fa90c21f9
ayoubouria <69218182+ayoubouria@users.noreply.github.com>
2023-10-30 17:08:04 +0000

    first commit

---
+++ lib/widgets/login_form.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/todo_page.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _email;
+  String? _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Email',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your email address';
+              }
+              return null;
+            },
+            onSaved: (value) {
+              _email = value;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Password',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+            onSaved: (value) {
+              _password = value;
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+                if (_email == 'admin' && _password == 'admin') {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const TodoPage()));
+                } else {
+                  _formKey.currentState!.save();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('User not found please register '),
+                    ),
+                  );
+                }
+              } else {
+                _formKey.currentState!.save();
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Enter valid email and password'),
+                  ),
+                );
+                // if (_formKey.currentState!.validate()) {
+                //   _formKey.currentState!.save();
+                //   ScaffoldMessenger.of(context).showSnackBar(
+                //     SnackBar(
+                //       content: Text('Email: $_email, Password: $_password'),
+                //     ),
+                //   );
+              }
+            },
+            child: const Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+}
